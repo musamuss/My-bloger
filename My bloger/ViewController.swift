9a9e49fac6ef99b638895ab39e92754a2034760b
musamuss <taller.97@mail.ru>
2019-05-30 01:43:23 +0300

    Initial commit

---
+++ My bloger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My bloger
+//
+//  Created by Муса Дениев on 30/05/2019.
+//  Copyright © 2019 musamuss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
